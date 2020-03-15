11ca6a1b17669d18a6b8373f66de34d8ec7f28d8
Jesen Tanadi <mail@jesentanadi.com>
2020-03-15 12:34:05 -0400

    add error handling; append descriptive file on request error

--- utils/fetchAndZip.js
+++ utils/fetchAndZip.js
@@ -1,16 +1,32 @@
 const https = require("https");
+const makeError = require("./makeError");
 
 module.exports = (url, zipArchive, fileName) => {
   return new Promise((resolve, reject) => {
     const req = https.request(url, res => {
-      if (res.statusCode && res.statusCode === 200) {
-        zipArchive.append(res, { name: fileName });
-        zipArchive.on("entry", () => {
-          resolve(fileName);
+      if (res.statusCode !== 200) {
+        zipArchive.append(`Error ${res.statusCode} downloading from ${url}`, {
+          name: fileName
         });
       } else {
-        resolve();
+        zipArchive.append(res, { name: fileName });
       }
+
+      zipArchive.on("entry", () => {
+        resolve(fileName);
+      });
+
+      zipArchive.on("warning", e => {
+        if (e.code === "ENOENT") {
+          console.err(e);
+        } else {
+          throw makeError(500, e);
+        }
+      });
+
+      zipArchive.on("error", e => {
+        throw makeError(500, e);
+      });
     });
 
     req.on("error", err => {
